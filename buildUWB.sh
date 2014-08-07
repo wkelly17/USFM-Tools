01acb398a9deaa90e54db45a3354710cdbb9e873
Jesse A. Griffin <jesse@tummy.com>
2014-08-07 14:11:26 -0600

    Added basic build script for UWB

---
+++ buildUWB.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2014 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+
+python transform.py --target=context    --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+#python transform.py --target=md         --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+#python transform.py --target=html       --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+#python transform.py --target=singlehtml --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+#python transform.py --target=reader     --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+#python transform.py --target=csv        --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+#python transform.py --target=ascii      --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+
