48a4f13583bc3f3944548069521507d332207acd
Jesse A. Griffin <jesse@tummy.com>
2014-08-07 14:48:36 -0600

    Added options to build script

--- buildUWB.sh
+++ buildUWB.sh
@@ -8,11 +8,64 @@
 #  Contributors:
 #  Jesse Griffin <jesse@distantshores.org>
 
-python transform.py --target=context    --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
-#python transform.py --target=md         --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
-#python transform.py --target=html       --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
-#python transform.py --target=singlehtml --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
-#python transform.py --target=reader     --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
-#python transform.py --target=csv        --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
-#python transform.py --target=ascii      --usfmDir=/tmp/UWB-USFM/ --builtDir=/tmp/UWB-v1/ --name=UWB-EN-v3_1
+PROGNAME="${0##*/}"
+EXPORT=0
+TOOLS=/var/www/vhosts/door43.org/tools
+USFMSRC=/tmp/UWB-USFM
+USFMSRC=/tmp/UWB-1book
 
+help() {
+    echo
+    echo "Publish unfoldingWord Bible"
+    echo
+    echo "Usage:"
+    echo "   $PROGNAME -v <version> -l <language> [-e <exportfromd43first>]"
+    echo "   $PROGNAME --help"
+    echo
+    exit 1
+}
+
+if [ $# -lt 1 ]; then
+    help
+fi
+while test -n "$1"; do
+    case "$1" in
+        --help|-h)
+            help
+            ;;
+        --version|-v)
+            VER=$2
+            shift
+            ;;
+        --language|-l)
+            LANG=$2
+            shift
+            ;;
+        --export|-e)
+            EXPORT=1
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            help
+            ;;
+    esac
+    shift
+done
+
+if [ $EXPORT -eq 1 ]; then
+    $TOOLS/uwb/dw2usfm_assembled.sh
+fi
+
+NAME="UWB-$LANG-v$VER-`date +%F`"
+USFMPUBDIR="/tmp/UWB-$LANG-v$VER"
+
+python transform.py --target=context    --usfmDir=$USFMSRC --builtDir=$USFMPUBDIR --name=$NAME
+#python transform.py --target=md         --usfmDir=$USFMSRC --builtDir=$USFMPUBDIR --name=$NAME
+#python transform.py --target=html       --usfmDir=$USFMSRC --builtDir=$USFMPUBDIR --name=$NAME
+#python transform.py --target=singlehtml --usfmDir=$USFMSRC --builtDir=$USFMPUBDIR --name=$NAME
+#python transform.py --target=reader     --usfmDir=$USFMSRC --builtDir=$USFMPUBDIR --name=$NAME
+#python transform.py --target=csv        --usfmDir=$USFMSRC --builtDir=$USFMPUBDIR --name=$NAME
+#python transform.py --target=ascii      --usfmDir=$USFMSRC --builtDir=$USFMPUBDIR --name=$NAME
+
+echo "See $USFMPUBDIR"
