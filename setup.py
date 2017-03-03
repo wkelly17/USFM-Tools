1088b1881d5d8f92541cd500ef52a6d4f035ac0f
Phil Hopper <philhopper@sibertec.com>
2017-03-03 11:07:09 -0500

    Fix for travis.ci (#32)

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
-import os
 from setuptools import setup
-
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(f_name):
-    return open(os.path.join(os.path.dirname(__file__), f_name)).read()
-
 
 setup(
     name="usfm_tools",
-    version="0.0.7",
+    version="0.0.8",
     author="unfoldingWord",
-    author_email="unfoldingword.org",
+    author_email="info@unfoldingWord.org",
     description="A framework for transforming .usfm files into specified targets",
     license="MIT",
     keywords="unfoldingWord usfm tools",
     url="https://github.com/unfoldingWord-dev/USFM-Tools",
     packages=['usfm_tools', 'usfm_tools/support'],
-    long_description=read('README.md'),
+    long_description='This project comprises a framework for transforming .usfm files into specified targets. It is '
+                     'primarily used for the Open English Bible, and may need adjustment if used for other purposes. '
+                     'This fork of USFM-Tools includes basic support for conversion to USX.',
     classifiers=[],
     requires=['pyparsing', ]
 )
